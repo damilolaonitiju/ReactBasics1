7a232a67942863262ddb65c117e282fc220615fd
Damilola <damilolaonitiju@gmail.com>
2024-05-05 09:25:18 +0100

    FIRST PROJECT

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,21 @@
+function ListGroup(){
+    return (
+      <>
+        <ul className="list-disc">
+          <li className="list-disc border-b">
+            Now this is a story all about how, my life got flipped-turned upside
+            down
+          </li>
+          <li className="list-disc border-b">
+            Now this is a story all about how, my life got flipped-turned upside
+            down
+          </li>
+          <li className="list-disc border-b">
+            Now this is a story all about how, my life got flipped-turned upside
+            down
+          </li>
+        </ul>
+      </>
+    );
+}
+export default ListGroup
